4e1578df319bea7b8a693ade16e19ac8105d2bb8
thieupepijn <thieupepijn@hotmail.com>
2020-11-18 21:33:29 +0100

    added function RemoveBracelets

--- Questions2Book/UtilText.cs
+++ Questions2Book/UtilText.cs
@@ -12,9 +12,32 @@ namespace Questions2Book
             string text = markdown.Replace("&#39;", "'");
             text = text.Replace("&gt;", string.Empty);
             text = text.Replace("&quot;", "\"");
+            text = text.Replace("&amp;", "&");
             text = text.Replace(Environment.NewLine + Environment.NewLine, Environment.NewLine);
             return text;
+        }
 
+        public static string RemoveBracelets(string text)
+        {
+            for (int counter=0;counter<100;counter++)
+            {
+                string line1 = string.Format("[{0}]", counter);
+                string line2 = string.Format("@@@{0}@@@", counter);
+                text = text.Replace(line1, line2);
+            }
+
+            text = text.Replace("[", string.Empty);
+            text = text.Replace("]", string.Empty);
+
+
+            for (int counter = 0; counter < 100; counter++)
+            {
+                string line1 = string.Format("@@@{0}@@@", counter);
+                string line2 = string.Format("[{0}]", counter);  
+                text = text.Replace(line1, line2);
+            }
+
+            return text;
         }
 
     }
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -12,5 +12,20 @@ namespace UnitTests
             string line = "I&#39;ve been thinking about you";
             Assert.AreEqual("I've been thinking about you", UtilText.MarkDown2Text(line));
         }
+
+        [TestMethod]
+        public void TestRemoveBracelets1()
+        {
+            string line = "hallo [alle] dames en heren";
+            Assert.AreEqual("hallo alle dames en heren", UtilText.RemoveBracelets(line));
+        }
+
+        [TestMethod]
+        public void TestRemoveBracelets2()
+        {
+            string line = "hallo [2] dames en heren";
+            Assert.AreEqual("hallo [2] dames en heren", UtilText.RemoveBracelets(line));
+        }
+
     }
 }
