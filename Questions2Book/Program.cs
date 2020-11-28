9e7c96a789023978b0506ed0edd30dfb01d16ed7
thieupepijn <thieupepijn@hotmail.com>
2020-11-28 22:06:24 +0100

    added function to number a pdf-document

--- Questions2Book/Program.cs
+++ Questions2Book/Program.cs
@@ -18,24 +18,44 @@ namespace Questions2Book
         {
             string userId = "115746";
             string siteName = "workplace";
-           
+            string bookFileName = "WorkplaceQuestions.pdf";
+            string bookFileNameNumbered = "WorkplaceQuestionsNumbered.pdf";
+
             List<Answer> answers = Answer.GetAnswers(userId, siteName);
-            Answer.WriteAnswerstoFile(answers);
-
             List<Question> questions = Question.GetQuestions(answers, siteName);
-            questions.ForEach(q => q.FindAnswer(answers));
+
+            WriteQuestions2Book(questions, bookFileName);
+            NumberPdfDocument(bookFileName, bookFileNameNumbered);
+        }
 
 
-            PdfDocument pdfDocument = new PdfDocument(new PdfWriter(new FileStream("WorkplaceQuestions.pdf", FileMode.Create, FileAccess.Write)));
+        private static void WriteQuestions2Book(List<Question> questions, string bookFileName)
+        {
+            PdfDocument pdfDocument = new PdfDocument(new PdfWriter(new FileStream(bookFileName, FileMode.Create, FileAccess.Write)));
             Document document = new Document(pdfDocument);
 
             string allText = Question.Question2String(questions);
             ConverterProperties converterProperties = new ConverterProperties();
             HtmlConverter.ConvertToPdf(allText, pdfDocument, converterProperties);
-
-
         }
 
+        private static void NumberPdfDocument(string inFilePath, string outFilePath)
+        {
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(inFilePath), new PdfWriter(outFilePath));
+            Document document = new Document(pdfDocument);
+
+            int numberOfPages = pdfDocument.GetNumberOfPages();
+            int pagewidth = Convert.ToInt16(pdfDocument.GetPage(numberOfPages).GetPageSize().GetWidth());
+
+            for (int i = 1; i <= numberOfPages; i++)
+            {
+
+                document.ShowTextAligned(new Paragraph(i.ToString()),
+                          pagewidth / 2, 25, i, TextAlignment.RIGHT, VerticalAlignment.TOP, 0);
+            }
+            document.Close();
+        }
+            
 
 
 
