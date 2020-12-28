df2326d84ccf3873187a5d8f180dfd917b2d46df
thieupepijn <thieupepijn@hotmail.com>
2020-12-29 00:14:39 +0100

    slight alterations for publishing with pumbo

--- Questions2Book/Program.cs
+++ Questions2Book/Program.cs
@@ -20,7 +20,7 @@ namespace Questions2Book
         {
             string userId = "115746";
             string siteName = "workplace";
-            string frontCoverFileName = @"..\Assets\ScrummingTheDayAwayVersion1\Cover.pdf";
+            string frontCoverFileName = @"..\Assets\ScrummingTheDayAwayVersion1\CoverA5.pdf";
             string frontCoverBackFileName = "secondPage";
             string questionsFileName = "Questions.pdf";
             string referencesFileName = "References.pdf";
@@ -41,17 +41,17 @@ namespace Questions2Book
             MakeBlankPage(blankPageFileName);
 
             List<string> sources = new List<string>();
-         //   sources.Add(frontCoverFileName);
+          //  sources.Add(frontCoverFileName);
             sources.Add(frontCoverBackFileName);
             sources.Add(questionsFileName);
             sources.Add(referencesFileName);
-            sources.Add(blankPageFileName);
-            sources.Add(blankPageFileName);
-            sources.Add(blankPageFileName);
+          //  sources.Add(blankPageFileName);
+         //   sources.Add(blankPageFileName);
+         //   sources.Add(blankPageFileName);
 
             UtilPDF.MergePdf(sources, bookFileName);
 
-            UtilPDF.NumberPdfDocument(bookFileName, bookFileNameNumbered, false);
+            UtilPDF.NumberPdfDocument(bookFileName, bookFileNameNumbered, true);
         }
 
         private static void WriteQuestionsToPDF(List<Question> questions, string questionsPdFFileName)
@@ -84,7 +84,7 @@ namespace Questions2Book
             string source = @"https://www.flickr.com/photos/84263554@N00/50382963456/in/photolist-2jLaRHJ-7ib7V9-nknW6J-5xjyd4-2inRnzM-2j1Gnqo-H6a2Rb-2k57AJP-24eYEMe-2j2nuX5-Jo8Nud-NP26SL-2gfMeVW-6qxvo2-uvtg31-2bsEgTD-TQ6e7h-NexjzR-73gx6e-DcG9wj-5tYc2M-esi8GU-36tUJr-2i6cD-8vGnwi-28tfqdu-2k53Mp6-p6MFqt-5Dw5v-2gKGJdS-CnvUbT-UmmP9X-2bu2kGM-NrW4AD-W61rLT-bDZAFD-2jupJLv-5n9Cyo-RwSnpY-5xVU7L-29JyniY-a1E7P5-QzqYnN-UTtmNy-24Feos-7ZCyAG-22KEjh5-XHif9b-FjiyYV-MxynM000000";
             string licence = @"Attribution-ShareAlike 2.0 Generic (CC BY-SA 2.0)";
             string newLine = "<BR>";
-            string text = string.Format("Image taken from {0} {0} {1} {0} {0} under the {2} license", 
+            string text = string.Format("Cover image taken from {0} {0} {1} {0} {0} under the {2} license", 
                           newLine, source, licence);
             UtilPDF.WriteHtmlText2Pdf(text, fileName);
         }
